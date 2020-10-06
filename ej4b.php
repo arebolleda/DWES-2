8ababc421838d10fe797d23a51065ebf8b90952f
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2020-10-06 13:53:55 +0200

    Add files via upload

---
+++ ej4b.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+   <title>Bucles 4</title>
+</head>
+<h1>Bucles 4: Tabla multiplicar</h1>
+<body>
+
+<?php
+
+    #Raquel Alcázar
+    
+    $num=8;
+
+    for($i=0; $i<=10; $i++){
+
+        echo $num ." x " .$i ." = " .($num*$i) ."</br>"; 
+    }
+?>
+</body>
+</html>
